9586e4abc222097bafdcc0fa00dafa3c4d0d781c
pwndl <pwindle11@gmail.com>
2023-11-07 23:40:31 +0000

    add help for me and adjust model names

--- types/openai.ts
+++ types/openai.ts
@@ -10,7 +10,7 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_3_5_preview = 'gpt-3.5-turbo-1106', // Remove this after 2023-12-11. Source https://openai.com/blog/new-models-and-developer-products-announced-at-devday#updated-gpt-3-5-turbo
+  GPT_3_5_PREVIEW = 'gpt-3.5-turbo-1106', // Remove this after 2023-12-11. Source https://openai.com/blog/new-models-and-developer-products-announced-at-devday#updated-gpt-3-5-turbo
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
   GPT_4_TURBO = 'gpt-4-1106-preview' // This is only a preview and will be replaced in the coming weeks (2023-11-07). Source: https://openai.com/blog/new-models-and-developer-products-announced-at-devday#gpt-4-turbo-with-128k-context
@@ -22,19 +22,19 @@ export const fallbackModelID = OpenAIModelID.GPT_3_5;
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_3_5]: {
     id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
+    name: 'GPT-3.5-TURBO',
     maxLength: 12000,
     tokenLimit: 4000,
   },
   [OpenAIModelID.GPT_3_5_AZ]: {
     id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
+    name: 'GPT-3.5-TURBO',
     maxLength: 12000,
     tokenLimit: 4000,
   },
-  [OpenAIModelID.GPT_3_5_preview]: { // Remove this after 2023-12-11. Source https://openai.com/blog/new-models-and-developer-products-announced-at-devday#updated-gpt-3-5-turbo
-    id: OpenAIModelID.GPT_3_5_preview,
-    name: 'GPT-3.5',
+  [OpenAIModelID.GPT_3_5_PREVIEW]: { // Remove this after 2023-12-11. Source https://openai.com/blog/new-models-and-developer-products-announced-at-devday#updated-gpt-3-5-turbo
+    id: OpenAIModelID.GPT_3_5_PREVIEW,
+    name: 'GPT-3.5-TURBO-preview',
     maxLength: 12000,
     tokenLimit: 4000,
   },
