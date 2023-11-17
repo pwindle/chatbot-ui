653ea2f57a1ef9494624b6e2702662a42e536e16
yifguoMSFT <yifguo@microsoft.com>
2023-11-16 17:23:47 -0800

    add image button

--- types/openai.ts
+++ types/openai.ts
@@ -13,7 +13,8 @@ export enum OpenAIModelID {
   GPT_3_5_PREVIEW = 'gpt-3.5-turbo-1106', // Remove this after 2023-12-11. Source https://openai.com/blog/new-models-and-developer-products-announced-at-devday#updated-gpt-3-5-turbo
   GPT_4 = 'gpt-4',
   GPT_4_32K = 'gpt-4-32k',
-  GPT_4_TURBO = 'gpt-4-1106-preview' // This is only a preview and will be replaced in the coming weeks (2023-11-07). Source: https://openai.com/blog/new-models-and-developer-products-announced-at-devday#gpt-4-turbo-with-128k-context
+  GPT_4_TURBO = 'gpt-4-1106-preview',
+  GPT_4_TURBO_VISION = 'gpt-4-vision-preview'
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
@@ -53,6 +54,12 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
   [OpenAIModelID.GPT_4_TURBO]: { // This is only a preview and will be replaced in the coming weeks (2023-11-07). Source: https://openai.com/blog/new-models-and-developer-products-announced-at-devday#gpt-4-turbo-with-128k-context
     id: OpenAIModelID.GPT_4_TURBO,
     name: 'GPT-4-TURBO',
+    maxLength: 384000,
+    tokenLimit: 128000,
+  },
+  [OpenAIModelID.GPT_4_TURBO_VISION]: {
+    id: OpenAIModelID.GPT_4_TURBO_VISION,
+    name: 'GPT-4-TURBO-VISION',
     maxLength: 384000,
     tokenLimit: 128000,
   },
