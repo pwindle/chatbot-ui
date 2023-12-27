d9699ae63161b0664314a674a9574b07ea27eca0
pwndl <pwindle11@gmail.com>
2023-12-27 01:03:29 +0000

    handle image drag and drop

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -37,6 +37,8 @@ interface Props {
   stopConversationRef: MutableRefObject<boolean>;
   textareaRef: MutableRefObject<HTMLTextAreaElement | null>;
   showScrollDownButton: boolean;
+  draggedImages: string[];
+  setDraggedImages: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
 export const ChatInput = ({
@@ -46,6 +48,8 @@ export const ChatInput = ({
   stopConversationRef,
   textareaRef,
   showScrollDownButton,
+  draggedImages,
+  setDraggedImages,
 }: Props) => {
   const { t } = useTranslation('chat');
 
@@ -66,6 +70,8 @@ export const ChatInput = ({
   const [plugin, setPlugin] = useState<Plugin | null>(null);
   const [images, setImages] = useState<string[]>([]);
   const imageInputRef = useRef<HTMLInputElement | null>(null);
+
+  const chatInputContainerRef = useRef<HTMLDivElement>(null);
 
   const handleImageInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -357,8 +363,16 @@ const handleSend = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (draggedImages.length > 0) {
+      setImages((prevImages) => [...prevImages, ...draggedImages]);
+      setDraggedImages([]); // Clear the dragged images state after integration
+    }
+  }, [draggedImages, setDraggedImages]);
+
   return (
-    <div className="absolute bottom-0 left-0 w-full border-transparent bg-gradient-to-b from-transparent via-white to-white pt-6 dark:border-white/20 dark:via-[#343541] dark:to-[#343541] md:pt-2">
+    <div ref={chatInputContainerRef} 
+    className="absolute bottom-0 left-0 w-full border-transparent bg-gradient-to-b from-transparent via-white to-white pt-6 dark:border-white/20 dark:via-[#343541] dark:to-[#343541] md:pt-2">
       <div className="stretch mx-2 mt-4 flex flex-row gap-3 last:mb-2 md:mx-4 md:mt-[52px] md:last:mb-6 lg:mx-auto lg:max-w-3xl">
         {messageIsStreaming && (
           <button
